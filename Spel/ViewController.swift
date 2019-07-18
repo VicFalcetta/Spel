16bd944dd98d87d278461e981740c1536daf3341
Victor Falcetta do Nascimento <vic.falcetis@gmail.com>
2019-07-18 15:15:40 -0300

    Initial Commit

---
+++ Spel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spel
+//
+//  Created by Victor Falcetta do Nascimento on 18/07/19.
+//  Copyright © 2019 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
